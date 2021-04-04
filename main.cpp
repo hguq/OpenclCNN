f1d695282d10cc110d564d48ad4c5ab17029f15a
Azazel <1600012423@pku.edu.cn>
2021-04-04 23:02:53 +0800

    Result for homework.

--- main.cpp
+++ main.cpp
@@ -18,87 +18,75 @@ void opencl_vec_add(int n) {
     cl_int ret_status;
     cl_platform_id platform;
     cl_device_id device;
-
     ret_status = clGetPlatformIDs(1, &platform, nullptr);
-    assert(!ret_status);
     ret_status = clGetDeviceIDs(platform, CL_DEVICE_TYPE_ALL, 1, &device, nullptr);
-    assert(!ret_status);
+    cl_context context = clCreateContext(nullptr, 1, &device, nullptr, nullptr, &ret_status);
+    auto command_queue = clCreateCommandQueue(context, device, CL_QUEUE_PROFILING_ENABLE, &ret_status);
 
-    auto context = clCreateContext(nullptr, 1, &device, nullptr, nullptr, &ret_status);
-    assert(!ret_status);
+    size_t buff_size = n * sizeof(int);
+    auto buff_a = clCreateBuffer(context, CL_MEM_READ_ONLY, buff_size, nullptr, &ret_status);
+    auto buff_b = clCreateBuffer(context, CL_MEM_READ_ONLY, buff_size, nullptr, &ret_status);
+    auto buff_c = clCreateBuffer(context, CL_MEM_WRITE_ONLY, buff_size, nullptr, &ret_status);
 
-    auto cmd_queue = clCreateCommandQueue(context, device, CL_QUEUE_PROFILING_ENABLE, &ret_status);
-    assert(!ret_status);
+    ret_status = clEnqueueWriteBuffer(command_queue, buff_a, CL_FALSE, 0, buff_size, A, 0, nullptr, nullptr);
+    ret_status = clEnqueueWriteBuffer(command_queue, buff_b, CL_FALSE, 0, buff_size, B, 0, nullptr, nullptr);
 
-    int buff_size = n * sizeof(int);
-    auto buffer_a = clCreateBuffer(context, CL_MEM_READ_ONLY, buff_size, nullptr, &ret_status);
-    assert(!ret_status);
-    auto buffer_b = clCreateBuffer(context, CL_MEM_READ_ONLY, buff_size, nullptr, &ret_status);
-    assert(!ret_status);
-    auto buffer_c = clCreateBuffer(context, CL_MEM_WRITE_ONLY, buff_size, nullptr, &ret_status);
-    assert(!ret_status);
-
-
-    // Use clEnqueueWriteBuffer() to write input.
-    ret_status = clEnqueueWriteBuffer(cmd_queue, buffer_a, CL_FALSE, 0, buff_size, A, 0, nullptr, nullptr);
-    assert(!ret_status);
-    ret_status = clEnqueueWriteBuffer(cmd_queue, buffer_b, CL_FALSE, 0, buff_size, B, 0, nullptr, nullptr);
-    assert(!ret_status);
-
-    // Read program source.
-    string programSource = readProgramFile("../vec_add.cl");
-
-    // Create a program using clCreateProgramWithSource()
-    cl_program program = clCreateProgramWithSource(context, 1, (const char **) &programSource, nullptr, &ret_status);
-    assert(!ret_status);
-
-    // Build (compile) the program for the devices
+    string src = readProgramFile("../vec_add.cl");
+    auto src_buff = new char[src.size() + 1];
+    src_buff[src.size()] = 0;
+    memcpy(src_buff, src.c_str(), sizeof(char) * src.size());
+    auto program = clCreateProgramWithSource(context, 1, (const char **) &src_buff, nullptr, &ret_status);
     ret_status = clBuildProgram(program, 1, &device, nullptr, nullptr, nullptr);
-    assert(!ret_status);
 
-    // Use clCreateKernel() to create a kernel.
     auto kernel = clCreateKernel(program, "vec_add", &ret_status);
-    assert(!ret_status);
 
-    // Set kernal arguments.
-    ret_status = clSetKernelArg(kernel, 0, sizeof(cl_mem), &buffer_a) |
-                 clSetKernelArg(kernel, 1, sizeof(cl_mem), &buffer_b) |
-                 clSetKernelArg(kernel, 2, sizeof(cl_mem), &buffer_c);
-    assert(!ret_status);
+    int work_item_len;
+    if (n > 1024 * 1024 * 64)work_item_len = n / (1024 * 1024 * 64);
+    else work_item_len = 1;
+    ret_status = clSetKernelArg(kernel, 0, sizeof(cl_mem), &buff_a) |
+                 clSetKernelArg(kernel, 1, sizeof(cl_mem), &buff_b) |
+                 clSetKernelArg(kernel, 2, sizeof(cl_mem), &buff_c);
 
-    // Configure the work-item structure
-    size_t work_size[0];
-    if (n <= 1024) work_size[0] = n, work_size[1] = work_size[2] = 1;
-    else if (n <= 1024 * 1024)work_size[0] = 1024, work_size[1] = n / 1024, work_size[2] = 1;
-    else work_size[0] = work_size[1] = 1024, work_size[2] = n / 1024 / 1024;
+    size_t work_size[3];
+    if (n <= 1024) {
+        work_size[0] = n;
+        work_size[1] = 1;
+        work_size[2] = 1;
+    } else if (n <= 1024 * 1024) {
+        work_size[0] = 1024;
+        work_size[1] = n / 1024;
+        work_size[2] = 1;
+    } else if (n <= 1024 * 1024 * 64) {
+        work_size[0] = 1024;
+        work_size[1] = 1024;
+        work_size[2] = n / 1024 / 1024;
+    } else {
+        cout << "N too big." << endl;
+        assert(1);
+        return;
+    }
 
-    // Execute the kernel by using clEnqueueNDRangeKernel().
+    clFinish(command_queue);
     cl_event kernel_exec_event;
-    ret_status = clEnqueueNDRangeKernel(cmd_queue, kernel, 3, nullptr, work_size, nullptr, 0, nullptr,
+
+    ret_status = clEnqueueNDRangeKernel(command_queue, kernel, 3, nullptr, work_size, nullptr, 0, nullptr,
                                         &kernel_exec_event);
-    assert(!ret_status);
-    cl_ulong op, ed;
-    clFinish(cmd_queue);
     clWaitForEvents(1, &kernel_exec_event);
+
+    cl_ulong op, ed;
     clGetEventProfilingInfo(kernel_exec_event, CL_PROFILING_COMMAND_START, sizeof(op), &op, nullptr);
     clGetEventProfilingInfo(kernel_exec_event, CL_PROFILING_COMMAND_END, sizeof(ed), &ed, nullptr);
-    cout << "Opencl time: " << double(ed - op) / (1e9) << "s" << endl;
+    cout << "Opencl time: " << double(ed - op) / 1e9 << "s" << endl;
 
+    clEnqueueReadBuffer(command_queue, buff_c, CL_TRUE, 0, buff_size, C_GPU, 0, nullptr, nullptr);
 
-    // Use clEnqueueReadBuffer() to read the OpenCL output
-    ret_status = clEnqueueReadBuffer(cmd_queue, buffer_c, CL_TRUE, 0, buff_size, C_GPU, 0, nullptr, nullptr);
-    assert(!ret_status);
-    clFinish(cmd_queue);
-
-    // Free OpenCL resources
     clReleaseKernel(kernel);
     clReleaseProgram(program);
-    clReleaseCommandQueue(cmd_queue);
-    clReleaseMemObject(buffer_a);
-    clReleaseMemObject(buffer_b);
-    clReleaseMemObject(buffer_c);
+    clReleaseCommandQueue(command_queue);
+    clReleaseMemObject(buff_a);
+    clReleaseMemObject(buff_b);
+    clReleaseMemObject(buff_c);
     clReleaseContext(context);
-
 }
 
 void cpu_vec_add(int N) {
--- vec_add.cl
+++ vec_add.cl
@@ -1,5 +1,5 @@
 __kernel void vec_add(__global int *A, __global int *B, __global int *C){
    int idx = get_global_id(0)*get_global_size(1)*get_global_size(2)+get_global_id(1)*get_global_size(2)+get_global_id(2);
-   C[idx] = A[idx] + B[idx];
- }                              
+       C[idx] = A[idx] + B[idx];
+ }
 
